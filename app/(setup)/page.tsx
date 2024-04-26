44883148389ed2047d1d7aaae9f4e344022d5869
jeong99 <jbh990621@gmail.com>
2024-04-26 13:20:50 +0900

    server 생성

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,11 +1,10 @@
-import { ModeToggle } from "@/components/mode-toggle";
+import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
 
 const SetupPage = async () => {
   const profile = await initialProfile();
-
   const server = await db.server.findFirst({
     where: {
       members: {
@@ -19,13 +18,7 @@ const SetupPage = async () => {
   if (server) {
     return redirect(`/servers/${server.id}`);
   }
-
-  return (
-    <div>
-      Create a Server
-      <ModeToggle />
-    </div>
-  );
+  return <InitialModal />;
 };
 
 export default SetupPage;
