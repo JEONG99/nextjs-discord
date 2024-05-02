775640376aec0a9f43bdcf3ad7bef13a06b4b4fd
jeong99 <jbh990621@gmail.com>
2024-05-02 15:24:54 +0900

    conversations 셋업

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,4 +1,5 @@
 import MobileToggle from "@/components/mobile-toggle";
+import UserAvatar from "@/components/user-avatar";
 import { Hash } from "lucide-react";
 
 interface ChatHeaderProps {
@@ -17,6 +18,9 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
       <MobileToggle serverId={serverId} />
       {type === "channel" && (
         <Hash className="mr-2 h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+      )}
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className="mr-2 h-8 w-8 md:h-8 md:w-8" />
       )}
       <p className="text-md font-semibold text-black dark:text-white">{name}</p>
     </div>
