46359e3dcafaeebb45d87be6f4768ff679081a7b
jeong99 <jbh990621@gmail.com>
2024-05-02 16:10:46 +0900

    소켓 셋업

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,4 +1,5 @@
 import MobileToggle from "@/components/mobile-toggle";
+import SocketIndicator from "@/components/socket-indicator";
 import UserAvatar from "@/components/user-avatar";
 import { Hash } from "lucide-react";
 
@@ -23,6 +24,9 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
         <UserAvatar src={imageUrl} className="mr-2 h-8 w-8 md:h-8 md:w-8" />
       )}
       <p className="text-md font-semibold text-black dark:text-white">{name}</p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
